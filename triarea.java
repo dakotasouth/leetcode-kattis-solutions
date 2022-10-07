81d0cceb680a02ee4c82585d5a4dca4d22e2e0ad
Dakota <southd20@gmail.com>
2022-10-07 11:46:49 -0500

    new problems

--- triarea.java
+++ triarea.java
@@ -1,10 +1,9 @@
 import java.util.*;
-public class adolescentarchitecture {
+
+public class triarea {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        
-        
+        System.out.println((sc.nextDouble() * sc.nextDouble()) / 2.0);
+
     }
 }
